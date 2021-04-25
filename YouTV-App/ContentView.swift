30c692304759ba4d7a92cf7ce115cbbe506210c0
Farshad Mousalou <farshadm90@gmail.com>
2021-04-25 21:00:03 +0430

    - Created Xcode Porject.
    (cherry picked from commit f5401870547f7be4f73a5c92c36c258afa457e2a)

---
+++ YouTV-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YouTV-App
+//
+//  Created by Farshad M on 4/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YouTV-App/YouTV_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTV_AppApp.swift
+//  YouTV-App
+//
+//  Created by Farshad M on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct YouTV_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
