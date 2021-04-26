aa332c75719ff0c98f2594c971d86f30f2f5d0f6
Farshad Mousalou <farshadm90@gmail.com>
2021-04-26 21:14:39 +0430

    - Added Bundle and Collection Extensions.

---
+++ YouTVDemo/Classes/Extensions/Foundation/Bundle+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  Bundle+Extension.swift
+//  YouTVDemo
+//
+//  Created by Farshad Mousalou on 4/26/21.
+//  Copyright © 2021 iFarshad. All rights reserved.
+//
+
+import Foundation
+
+extension Bundle {
+
+
+    /// The Release Version Value known as Marketing Versions. like 1.0
+    var releaseVersionNumber: String? {
+        return self.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String
+    }
+
+    /// The Bundle Version Value known as Build Versions. like 1.0
+    var buildVersionNumber: String? {
+        return self.object(forInfoDictionaryKey: "CFBundleVersion") as? String
+    }
+
+}
