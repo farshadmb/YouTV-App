6ade960a341fe858541a7870af995beea6815480
Farshad Mousalou <farshadm90@gmail.com>
2021-05-06 23:57:02 +0430

    - Added Empty Show and Movie Cells.

---
+++ YouTVDemo/Classes/Presentation/Commons/Views/Movies/MovieCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCollectionViewCell.swift
+//  YouTVDemo
+//
+//  Created by Farshad Mousalou on 5/6/21.
+//  Copyright © 2021 iFarshad. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ YouTVDemo/Classes/Presentation/Commons/Views/Shows/ShowCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ShowCollectionViewCell.swift
+//  YouTVDemo
+//
+//  Created by Farshad Mousalou on 5/6/21.
+//  Copyright © 2021 iFarshad. All rights reserved.
+//
+
+import UIKit
+
+class ShowCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
