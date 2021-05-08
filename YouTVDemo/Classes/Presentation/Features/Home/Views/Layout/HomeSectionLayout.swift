31b6c1cb48bf141db9af3f80737d05dcb20caf2b
Farshad Mousalou <farshadm90@gmail.com>
2021-05-08 06:55:00 +0430

    - Added `HomeSectionLayout` abstract.

---
+++ YouTVDemo/Classes/Presentation/Features/Home/Views/Layout/HomeSectionLayout.swift
@@ -0,0 +1,16 @@
+//
+//  HomeSectionLayout.swift
+//  YouTVDemo
+//
+//  Created by Farshad Mousalou on 5/8/21.
+//  Copyright © 2021 iFarshad. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol HomeSectionLayout {
+    
+    func sectionLayout() -> NSCollectionLayoutSection
+    
+}
