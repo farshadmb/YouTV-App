8dba00e5de29e5b9201aa13d980d7dc9283a8be1
Farshad Mousalou <farshadm90@gmail.com>
2021-05-08 07:34:11 +0430

    - Added `RxSwift` extensions.

---
+++ YouTVDemo/Classes/Extensions/RxSwift/RxSwift+Unwrap.swift
@@ -0,0 +1,25 @@
+//
+//  RxSwift+Unwrap.swift
+//  YouTVDemo
+//
+//  Created by Farshad Mousalou on 5/8/21.
+//  Copyright © 2021 iFarshad. All rights reserved.
+//
+
+import Foundation
+
+#if canImport(RxSwift)
+import RxSwift
+#endif
+
+extension ObservableType {
+
+    /**
+     Takes a sequence of optional elements and returns a sequence of non-optional elements, filtering out any nil values.
+     - returns: An observable sequence of non-optional elements
+     */
+
+    public func unwrap<Result>() -> Observable<Result> where Element == Result? {
+        return self.compactMap { $0 }
+    }
+}
---
+++ YouTVDemo/Classes/Extensions/RxSwift/Single+Additionals.swift
@@ -0,0 +1,57 @@
+//
+//  Single+Additionals.swift
+//  YouTVDemo
+//
+//  Created by Farshad Mousalou on 5/8/21.
+//  Copyright © 2021 iFarshad. All rights reserved.
+//
+
+import Foundation
+#if canImport(RxSwift)
+import RxSwift
+#endif
+
+extension ObservableType {
+
+    public func flatMapSingle<Result>(_ selector: @escaping (Element) throws -> Single<Result>) -> Single<Result> {
+        let single = Single.from(observable: self)
+        return single.flatMap(selector)
+    }
+
+    public func compactFirst() -> Single<Element> {
+        return first().asObservable().unwrap().asSingle()
+    }
+
+}
+
+extension PrimitiveSequenceType where Trait == SingleTrait {
+
+    public static func from<T: ObservableType>(observable: T) -> Single<T.Element> where Element == T.Element {
+        return .create { single in
+
+            var hasElement = false
+            let observer = observable.subscribe { event in
+                switch event {
+                case .next(let element):
+                    if hasElement {
+                        single(.failure(RxError.moreThanOneElement))
+                        return
+                    }
+                    hasElement = true
+                    single(.success(element))
+                case .error(let error):
+                    single(.failure(error))
+                case .completed:
+                    if !hasElement {
+                        single(.failure(RxError.noElements))
+                    }
+                }
+            }
+
+           return Disposables.create {
+            observer.dispose()
+           }
+       }
+    }
+
+}
