2eb9d05826a292d74bbfd52954a8df47c1af27ba
Farshad Mousalou <farshadm90@gmail.com>
2021-05-11 22:08:52 +0430

    - Modified `HomeSectionLayout`.

--- YouTVDemo/Classes/Presentation/Features/Home/Views/Layout/HomeSectionLayout.swift
+++ YouTVDemo/Classes/Presentation/Features/Home/Views/Layout/HomeSectionLayout.swift
@@ -9,8 +9,95 @@
 import Foundation
 import UIKit
 
+/// <#Description#>
+private let defaultWidthSize: CGFloat = 140.0
+
+/// <#Description#>
+private let defaultAspectRatio: CGFloat = 1.77
+
+/// <#Description#>
+let movieWidthSize: CGFloat = 150.0
+
+/**
+ <# Property Summery Here#>
+ */
 protocol HomeSectionLayout {
-    
+
+    /**
+     <# Property Summery Here#>
+     */
     func sectionLayout() -> NSCollectionLayoutSection
     
+}
+
+extension HomeSectionLayout {
+
+    /**
+     <# Property Summery Here#>
+     */
+    func headerElement() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                heightDimension: .absolute(44))
+
+        let headerElement = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize,
+                                                                        elementKind: UICollectionView.elementKindSectionHeader,
+                                                                        alignment: .topLeading)
+
+        return headerElement
+    }
+
+    /**
+     <# Property Summery Here#>
+     */
+    func movieItem() -> NSCollectionLayoutItem {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalWidth(1.77))
+
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 16, leading: 0, bottom: 16, trailing: 0)
+
+        return item
+    }
+
+    /**
+     <# Property Summery Here#>
+     */
+    func showItem() -> NSCollectionLayoutItem {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalWidth(1.77))
+
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 16, leading: 0, bottom: 16, trailing: 0)
+
+        return item
+    }
+
+    /**
+     <# Property Summery Here#>
+     */
+    func popularShowItem() -> NSCollectionLayoutItem {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .fractionalWidth(0.576_119_403))
+
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 16, leading: 0, bottom: 16, trailing: 0)
+
+        return item
+    }
+
+    /**
+     <# Property Summery Here#>
+     - Parameters:
+       - width: <#width description#>
+       - height: <#height description#>
+     - Returns: <#description#>
+     */
+    func groupSize(width: NSCollectionLayoutDimension = .absolute(defaultWidthSize),
+                   height: NSCollectionLayoutDimension = .estimated(defaultWidthSize * defaultAspectRatio)) -> NSCollectionLayoutSize {
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: width,
+                                               heightDimension: height)
+        return groupSize
+    }
+
 }
